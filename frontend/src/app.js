47f71218bf6808c9350bd22b233fda1d87e48b6e
Alex Aguirre <teamaguirre@gmail.com>
2023-01-01 13:07:16 -0800

    Created the profile page (no data pulled yet)

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
-import UserData from './pages/UserData';
+import UserData from './components/UserData';
 import CurrentUserProvider from './context/CurrentUser';
 
 
@@ -16,7 +16,7 @@ function App() {
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path='/signup' element={<Signup/>}/>
         <Route exact path='/profile' element={<Profile/>}/>
-        <Route exact path='/userdata' element={<UserData />}/>
+        <Route exact path='/userdata' element={<UserData/>}/>
       </Routes>
     </CurrentUserProvider>
   );
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,24 +2,23 @@ import React, { useContext} from 'react';
 import { CurrentUser } from '../context/CurrentUser'; // import the CurrentUser
 import Navbar from '../components/Navbar';
 import Login from '../pages/Login'
-import UserData from './UserData';
+import UserData from '../components/UserData';
 
 const Profile = () => {
    const { currentUser } = useContext(CurrentUser); // get the currentUser from the context
-   
 
     return (
         <div>
             <Navbar/>
             {currentUser ? (
-                <div>
+            <div>
                     <h1 id='greet' className='fw-bold'>Welcome, {currentUser.user_f_name}!</h1>
                     <h4 id='greet' className='fw-bold'>Let's get FIT!!</h4>
                    
-                 <UserData/> 
-                </div>  
+            <UserData/> 
+            </div>  
             ) : (
-                <Login />
+            <Login />
             )}
         </div>
     );
