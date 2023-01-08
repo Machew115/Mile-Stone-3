0de956e83376e8f5fb05a374e36047b99025ddf8
Gustavo <gustavo_roger@yahoo.com>
2023-01-08 04:43:14 -0800

    styling added to meal form

--- frontend/src/components/MealForm.js
+++ frontend/src/components/MealForm.js
@@ -38,59 +38,69 @@ function MealForm(props) {
     };
 
     return (
-        <div className="modal fade" id="form-modal" tabIndex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
+        <div className="modal fade w-100 position-absolute top-50 start-50 translate-middle" id="form-modal" tabIndex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
+            <div className="modal-dialog w-50">
+                <div className="modal-content w-100">
                 <div className="modal-header">
                     <h2 className="modal-title fs-5 fw-bold" id="formModalLabel">New Meal</h2>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div className="modal-body">
-                    <form className="add-form" onSubmit={handleSubmit}>
-                        <label htmlFor="description" className="fw-bold">Description:</label>
-                        <input
-                            type="text"
-                            id="description"
-                            className="w-50"
-                            value={description}
-                            onChange={(event) => setDescription(event.target.value)}
-                        />
+                <div className="modal-body w-100">
+                    <form className="add-form w-50" onSubmit={handleSubmit}>
+                        <div className='w-100 input-hold'>
+                            <input
+                                placeholder=' '
+                                type="text"
+                                id="description"
+                                className="w-100 input_"
+                                onChange={(event) => setDescription(event.target.value)}
+                            />
+                            <label htmlFor="description" className="fw-bold _label">Description:</label>
+                        </div>
                         <br />
-                        <label htmlFor="calories" className="fw-bold">Calories:</label>
-                        <input
-                            type="number"
-                            id="calories"
-                            className="w-50"
-                            value={calories}
-                            onChange={(event) => setCalories(event.target.value)}
-                        />
+                        <div className='w-100 input-hold'>
+                            <input
+                                placeholder=' '
+                                type="number"
+                                id="calories"
+                                className="w-100 input_"
+                                onChange={(event) => setCalories(event.target.value)}
+                            />
+                            <label htmlFor="calories" className="fw-bold _label">Calories (kcal):</label>
+                        </div>
                         <br />
-                        <label htmlFor="protein" className="fw-bold">Protein (g):</label>
-                        <input
-                            type="number"
-                            id="protein"
-                            className="w-50"
-                            value={protein}
-                            onChange={(event) => setProtein(event.target.value)}
-                        />
+                        <div className='w-100 input-hold'>
+                            <input
+                                placeholder=' '
+                                type="number"
+                                id="protein"
+                                className="w-100 input_"
+                                onChange={(event) => setProtein(event.target.value)}
+                            />
+                            <label htmlFor="protein" className="fw-bold _label">Protein (g):</label>
+                        </div>
                         <br />
-                        <label htmlFor="fat" className="fw-bold">Fat (g):</label>
-                        <input
-                            type="number"
-                            id="fat"
-                            className="w-50"
-                            value={fat}
-                            onChange={(event) => setFat(event.target.value)}
-                        />
+                        <div className='w-100 input-hold'>
+                            <input
+                                placeholder=' '
+                                type="number"
+                                id="fat"
+                                className="w-100 input_"
+                                onChange={(event) => setFat(event.target.value)}
+                            />
+                            <label htmlFor="fat" className="fw-bold _label">Fat (g):</label>
+                        </div>
                         <br />
-                        <label htmlFor="carbs" className="fw-bold">Carbs (g):</label>
-                        <input
-                            type="number"
-                            id="carbs"
-                            className="w-50"
-                            value={carbs}
-                            onChange={(event) => setCarbs(event.target.value)}
-                        />
+                        <div className='w-100 input-hold'>
+                            <input
+                                placeholder=' '
+                                type="number"
+                                id="carbs"
+                                className="w-100 input_"
+                                onChange={(event) => setCarbs(event.target.value)}
+                            />
+                            <label htmlFor="carbs" className="fw-bold _label">Carbs (g):</label>
+                        </div>
                         <br />
                         <button type="submit" className='btn btn-secondary fw-bold'>Add Meal</button>
                     </form>
