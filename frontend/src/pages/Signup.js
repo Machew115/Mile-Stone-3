7cb0d63fe3c31da18af0907114d59c20b43ede73
Gustavo <gustavo_roger@yahoo.com>
2023-01-08 18:02:55 -0800

    signup form styling updated

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -14,6 +14,9 @@ function Signup() {
 
     const[error, setError] =useState(null)
 
+    // Declare a state variable for the checkbox and set its initial value to false
+    const [showPassword, setShowPassword] = useState(false);   
+
     async function handleSubmit(e){
         e.preventDefault();
         
@@ -35,41 +38,62 @@ function Signup() {
     }
       
     return (
-        <form onSubmit={handleSubmit}>
-         {error && <div>{error}  <a href="/login"><button type='button'>Login</button></a></div>}   
-        <label htmlFor="user_f_name">First Name:</label>
-        <input
-            type="text"
-            name="user_f_name"
-            id="user_f_name"
-            value={user.user_f_name}
-            onChange={e=>setUser({...user,user_f_name:e.target.value})}
-        />
-        <label htmlFor="user_l_name">Last Name:</label>
-        <input
-            type="text"
-            name="user_l_name"
-            id="user_l_name"
-            value={user.user_l_name}
-            onChange={e=>setUser({...user,user_l_name:e.target.value})}
-        />
-        <label htmlFor="user_email">Email:</label>
-        <input
-            type="text"
-            name="user_email"
-            id="user_email"
-            value={user.user_email}
-            onChange={e=>setUser({...user,user_email:e.target.value})}
-        />
-        <label htmlFor="user_password">Password:</label>
-        <input
-            type="text"
-            name="user_password"
-            id="user_password"
-            value={user.user_password}
-            onChange={e=>setUser({...user,user_password:e.target.value})}
-        />
-        <button type="submit">Sign up</button>
+        <form className='add-form w-50 mt-4' onSubmit={handleSubmit}>
+            <h1> Sign Up </h1>
+            {error && <div>{error}  <a href="/login"><button type='button'>Login</button></a></div>}   
+            <div className='input-hold w-50 mt-3'>
+                <input
+                    className='w-100 input_'
+                    placeholder=' '
+                    type="text"
+                    name="user_f_name"
+                    id="user_f_name"
+                    onChange={e=>setUser({...user,user_f_name:e.target.value})}
+                />
+                <label className='fw-bold _label' htmlFor="user_f_name">First Name:</label>
+            </div>
+            <br />
+            <div className='input-hold w-50 mt-3'>
+                <input
+                    className='w-100 input_'
+                    placeholder=' '
+                    type="text"
+                    name="user_l_name"
+                    id="user_l_name"
+                    onChange={e=>setUser({...user,user_l_name:e.target.value})}
+                />
+                <label className='fw-bold _label' htmlFor="user_l_name">Last Name:</label>
+            </div>
+            <br />
+            <div className='input-hold w-50 mt-3'>
+                <input
+                    className='w-100 input_'
+                    placeholder=' '
+                    type="text"
+                    name="user_email"
+                    id="user_email"
+                    onChange={e=>setUser({...user,user_email:e.target.value})}
+                />
+                <label className='fw-bold _label' htmlFor="user_email">Email:</label>
+            </div>
+            <br />
+            <div className='input-hold w-50 mt-3'>
+                <input
+                    className='w-100 input_'
+                    placeholder=' '
+                    type={showPassword ? "text" : "password"} 
+                    name="user_password"
+                    id="user_password"
+                    onChange={e=>setUser({...user,user_password:e.target.value})}
+                />
+                <label className='fw-bold _label' htmlFor="user_password">Password:</label>
+            </div>
+            {/* Add a checkbox input field and bind it to the showPassword state variable */}
+            <div className='show-pw w-50 mt-1'>
+                <input type="checkbox" id='show-pw' checked={showPassword} onChange={e => setShowPassword(e.target.checked)} />
+                <label htmlFor='show-pw' className='px-2'>Show Password</label>
+            </div>
+            <button className='btn btn-secondary mt-5 w-50 fw-bold' type="submit">Sign up</button>
         </form>
     );
 };
