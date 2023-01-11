99e69698fcff07bed251b7dc766220f0f44ba9fb
Alex Aguirre <teamaguirre@gmail.com>
2023-01-10 19:15:56 -0800

    edited the README file and style the Error 404 page

--- frontend/src/pages/Error404.js
+++ frontend/src/pages/Error404.js
@@ -1,9 +1,10 @@
 function Error404() {
     return (
-        <div>
-            <h1 >404: PAGE NOT FOUND</h1>
-            <img width='100px' height='100px'src="../heart-beat-icon.png" alt="logo" />
-            <p>Oops, sorry, we can't find this page!</p>
+        <div style={{textAlign:'center', marginTop: '25px'}}>
+            <h1>404: PAGE NOT FOUND</h1>
+            <img style={{width:'200px', height:'250px'}}src="../gains-loading.jpg" alt="gains loading " />
+            <h4 style={{marginBottom:'25px'}}>Oops, having trouble lifting this page onto the app!</h4>
+            <a href="./" ><button className="btn btn-secondary">Let's Get Back to Tracking</button></a>
         </div>
     );
 }
